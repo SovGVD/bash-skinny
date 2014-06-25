ada2cae68f05e60d1443fe30d2e372691ec55c99
Gleb Devyatkin <sovgvd@gmail.com>
2014-06-25 19:34:26 +1200

    skinny update ther web site

--- skinny.sh
+++ skinny.sh
@@ -1,6 +1,4 @@
 #/bin/bash
 LOGIN="YOUR_LOGIN"
 PASSWORD="YOUR_PASSWORD"
-f_secs=`wget -q --header="accept-encoding: gzip" -O - https://skinny.co.nz/ |grep sec|grep input|cut -d\" -f8`
-IFS=' ' read -ra f_sec <<< "$f_secs"
-curl -s -X POST --data "mobile=$LOGIN&password=$PASSWORD&login=1&sec_old=${f_sec[0]}&sec=${f_sec[1]}" https://skinny.co.nz/2341_EN.htm |grep 'Credit remaining\|Calls'| sed 's/<tr>/\n/g' | sed 's/<\/td><td>/ /g' | sed 's/<[^>]\+>//g' | sed 's/^[ \t]*//g'
+curl -s -b -X POST --location --data "AuthenticationMethod=MemberAuthenticator&Email=$LOGIN&Password=$PASSWORD&BackURL=dashboard/&Remember=&action_dologin=Log%20in" https://www.skinny.co.nz/Security/LoginForm | grep -A 3 'balance_summaries\|balance_title\|amount"><sup>' | sed 's/<\/sup>/\n/g' | sed 's/<\/p>/\n/g' | grep -v '<\|--' | sed -e 's/^[ \t]*//' | sed '/^$/d'
