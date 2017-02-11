6f2182b60bea63b2edf1102950fe531302b4d21e
Gleb Devyatkin <sovgvd@gmail.com>
2017-02-11 13:07:27 +1300

    update for 2017-02-11 web site version
    I'm in New Zealand again and looks like old bash script for skinny does not work well

--- skinny.sh
+++ skinny.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 LOGIN="YOUR_LOGIN"
 PASSWORD="YOUR_PASSWORD"
-curl -s -b -X POST --location --data "AuthenticationMethod=MemberAuthenticator&Email=$LOGIN&Password=$PASSWORD&BackURL=dashboard/&Remember=&action_dologin=Log%20in" https://www.skinny.co.nz/Security/LoginForm | grep -A 5 'balance_summaries\|balance_title\|amount"><sup>' | sed 's/<\/sup>/\n/g' | sed 's/<\/p>/\n/g' | grep -v '<\|--' | sed -e 's/^[ \t]*//' | sed '/^$/d'
+curl -s -b -X POST --location --data "AuthenticationMethod=MemberAuthenticator&Email=$LOGIN&Password=$PASSWORD&LoginType=skinny-mobile&BackURL=dashboard%2F&action_dologin=Log+in" https://www.skinny.co.nz/Security/LoginForm | grep -A 17 'data-title'|grep 'last' | sed 's/ will expire on//g' | sed 's/<br \/>/\n   /g' | sed 's/<\/p>/\n/g' | sed 's/<[^>]\+>//g'
